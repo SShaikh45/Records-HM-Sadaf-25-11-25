38296008452b940601dca00a3d5a683faa81f318
Sadaf Shaikh <22SShaikh@heckgrammar.co.uk>
2025-11-25 22:00:04 +0000

    23

--- Program.cs
+++ Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        struct People
+        {
+            public string name;
+            public int age;
+            public string city;
+            public double height;
+            public string occupation;
+
+        }
+
+        public People(string pname, int page, string pcity, double pheight, string poccupation)
+        { 
+            name = pname;
+            age = page;
+            city = pcity;
+            height = pheight;
+            occupation = poccupation;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            People p;
+            p.name = "Sadaf";
+            p.age = 16;
+            p.city = "London";
+            p.height = 6.2;
+            p.occupation = "Student";
         }
+
+        People people2 = new People("Daniel", 12, "Swansea", 12.5, "Student");
     }
 }
