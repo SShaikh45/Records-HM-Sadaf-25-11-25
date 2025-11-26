3001dcdf87c1924ca846af0931c3a69cb1c8b025
SShaikh45 <22SShaikh@heckgrammar.co.uk>
2025-11-26 12:46:58 +0000

    qwerty123
    Added comments to clarify the purpose of various code sections and improve code readability.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
             public string city;
             public double height;
             public string occupation;
-
+// Adding fields to the record
         }
 
         public People(string pname, int page, string pcity, double pheight, string poccupation)
@@ -19,6 +19,7 @@
             city = pcity;
             height = pheight;
             occupation = poccupation;
+            //adding properties to the record?
         }
         static void Main(string[] args)
         {
@@ -35,7 +36,7 @@
             int total_age = 0;
             string[] total_city = new string[9];
             string[] total_occupation = new string[9];
-            
+            //making variables which can be used to add the specific types of input form the user as we go along (the for loop).
             People[] myPeople = new People[9];
             for (int i = 0; i < 9; i ++)
             {
@@ -54,6 +55,11 @@
                 Console.WriteLine("Enter the persons occupation");
                 myPeople[i].occupation = Console.ReadLine();
                 total_occupation[i]= myPeople[i].occupation;
+
+                //Adds the specific input to each specific variable to get the overall total for each input.
+                //Adds the data to each record within the array of records, which has 9 free elements.
+                //coukld make the code more efficient by reducing the ampunt of code associated with the total- variables and instead hust output each variable and then add up their total individually.
+                //could use a foreach loop next time to use different programming techniques.
             }
 
             Console.WriteLine("The total height of everyone is: " + total_height);
@@ -62,6 +68,9 @@
             Console.WriteLine("The average age of everyone is: " + total_age/9);
             Console.WriteLine("The list of everyone's cities are: " + String.Join(" ", total_city));
             Console.WriteLine("The list of everyone's occupations are: "+ String.Join(", ", total_occupation));
+            //Outputs the total and average specific inputs.
+            //Need to research more on array techniques, such as String.Join(), as it would enable me to manipulate arrays (specifically arrays of strings) better.
+            //note to self - Code guides for c# can be found on my own teams chat (to myself).
             
         }
 
