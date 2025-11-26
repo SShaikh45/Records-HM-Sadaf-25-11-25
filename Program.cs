04fdc1a99ed4269284f3d7f174416fabdda8176a
SShaikh45 <22SShaikh@heckgrammar.co.uk>
2025-11-26 12:35:45 +0000

    qwerty
    Added functionality to store and display cities and occupations of people.

--- Program.cs
+++ Program.cs
@@ -28,11 +28,13 @@
             p.city = "London";
             p.height = 6.2;
             p.occupation = "Student";
-             People people2 = new People("Daniel", 12, "Swansea", 12.5, "Student");
-        //makes a new people variable using the new keyword
+            People people2 = new People("Daniel", 12, "Swansea", 12.5, "Student");
+            //makes a new people variable using the new keyword
             
             double total_height = 0;
             int total_age = 0;
+            string[] total_city = new string[9];
+            string[] total_occupation = new string[9];
             
             People[] myPeople = new People[9];
             for (int i = 0; i < 9; i ++)
@@ -45,16 +47,22 @@
                 total_age = total_age + myPeople[i].age;
                 Console.WriteLine("Enter the person's city");
                 myPeople[i].city = Console.ReadLine();
+                total_city[i] = myPeople[i].city;
                 Console.WriteLine("Enter the persons height");
                 myPeople[i].height = Convert.ToDouble(Console.ReadLine());
                 total_height = total_height + Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Enter the persons occupation");
                 myPeople[i].occupation = Console.ReadLine();
+                total_occupation[i]= myPeople[i].occupation;
             }
 
             Console.WriteLine("The total height of everyone is: " + total_height);
             Console.WriteLine("The average height of everyone is " + total_height/9);
-            Console.WriteLine(");
+            Console.WriteLine("The total age of everyone is: " + total_age);
+            Console.WriteLine("The average age of everyone is: " + total_age/9);
+            Console.WriteLine("The list of everyone's cities are: " + String.Join(" ", total_city));
+            Console.WriteLine("The list of everyone's occupations are: "+ String.Join(", ", total_occupation));
+            
         }
 
        
